dfcfc2db44b921ab7df59efaad07851108363b84
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2018-08-06 18:50:51 -0400

    Checkpoint 8 Player Bar-Buttons: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import albumData from './../data/albums.js';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -45,16 +46,16 @@ class Album extends Component {
             }
         }
         
-            handleHoverOn(song) {
+        handleHoverOn(song) {
                 this.setState({hovered: song});
             }
             
-            handleHoverOff(song) {
+        handleHoverOff(song) {
                 this.setState({hovered: null});
     
             }
 
-            btnHandler(song, index) {
+        btnHandler(song, index) {
                 const playBtn = <ion-icon name="play"></ion-icon>;
                 const pauseBtn = <ion-icon name="pause"></ion-icon>;
                 const isSameSong = this.state.currentSong === song;
@@ -66,42 +67,56 @@ class Album extends Component {
                     return pauseBtn;
                   } else {
                     return (index +1);
-                  }
+                }
+            }
 
+        handlePrevClick() {
+            const currentIndex= this.state.album.songs.findIndex(song => this.state.currentSong === song);
+            const newIndex = Math.max(0,currentIndex -1);
+            const newSong = this.state.album.songs[newIndex];
+            this.setSong(newSong);
+            this.play();
         }
+            
 
-    render(){
-        return(
-          <section className='album'>
-            <section id="album-info">
-                <img id="album-cover art" src= {this.state.album.albumCover} alt={this.state.album.title}/>
-                <div className="album-details">
-                    <h1 id="album-title">{this.state.album.title}</h1>
-                    <h2 id className="artist">{this.state.album.artist}</h2>
-                    <div id="release-info">{this.state.album.releaseInfo}</div>
-                </div>
+        render(){
+            return(
+            <section className='album'>
+                <section id="album-info">
+                    <img id="album-cover art" src= {this.state.album.albumCover} alt={this.state.album.title}/>
+                    <div className="album-details">
+                        <h1 id="album-title">{this.state.album.title}</h1>
+                        <h2 id className="artist">{this.state.album.artist}</h2>
+                        <div id="release-info">{this.state.album.releaseInfo}</div>
+                    </div>
+                </section>
+                <table id="song-list">
+                    <colgroup>
+                        <col id="song-number-column" />
+                        <col id="song-title-column" />
+                        <col id="song-duration-column" />
+                    </colgroup>
+                    <tbody>
+                        {this.state.album.songs.map((songs,index) =>
+                            <tr className="song" key={index} onClick={()=> this.handleSongClick(songs)}  onMouseEnter={() => this.handleHoverOn(songs)} onMouseLeave={() => this.handleHoverOff(songs)}>
+                                <td> {this.btnHandler(songs, index)} </td>
+                                <td className="song-title">{songs.title}</td>
+                                <td className="song-duration">{songs.duration}</td>
+                            </tr>
+                            )}
+
+                    </tbody>
+                </table>
+                < PlayerBar 
+                    isPlaying={this.state.isPlaying} 
+                    currentSong={this.state.currentSong}
+                    handleSongClick ={()=>this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick = {()=> this.handlePrevClick()}
+                />
             </section>
-            <table id="song-list">
-                <colgroup>
-                    <col id="song-number-column" />
-                    <col id="song-title-column" />
-                    <col id="song-duration-column" />
-                </colgroup>
-                <tbody>
-                    {this.state.album.songs.map((songs,index) =>
-                          <tr className="song" key={index} onClick={()=> this.handleSongClick(songs)}  onMouseEnter={() => this.handleHoverOn(songs)} onMouseLeave={() => this.handleHoverOff(songs)}>
-                            <td> {this.btnHandler(songs, index)} </td>
-                            <td className="song-title">{songs.title}</td>
-                            <td className="song-duration">{songs.duration}</td>
-                          </tr>
-                        )}
-
-                </tbody>
-            </table>
-         </section>
-        );
+            );
+        }
     }
-}
 
 
 
---
+++ src/components/PlayerBar.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class PlayerBar extends Component {
+    render(){
+        return(
+        <section className="player-bar">
+                <section id="buttons">
+                <button id="previous" onClick={this.props.handlePrevClick}>
+                    <ion-icon name="skip-backward"></ion-icon>
+                </button>
+                <button id="play-pause" onClick={this.props.handleSongClick}>
+                    {this.props.isPlaying? 
+                    <ion-icon name="pause"></ion-icon>
+                    :
+                    <ion-icon name="play"></ion-icon>
+                    }
+                </button>
+                <button id="next">
+                    <ion-icon name="skip-forward"></ion-icon>
+                </button>
+            </section>
+            <section id="time-control">
+                <div className="current-time">-:-</div>
+                <input type="range" className="seek-bar" value="0"/>
+                <div className="total-time">-:-</div>
+            </section>
+            <section id="volume-control">
+                <div className="icon ion-volume-low"></div>
+                <input type="range" className="seek-bar" value="80"/>
+            </section>
+
+       </section>
+        );
+    }
+}
+
+export default PlayerBar;
